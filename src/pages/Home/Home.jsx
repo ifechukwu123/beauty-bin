a3c6a5a1a0d1cadd18c347d2f03581a7f084d548
Ife Onuorah <ifeonuorah@gmail.com>
2025-07-25 15:02:59 -0300

    Make homepage responsive for all screen sizes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import inventorySticker from "../../assets/stickers/inventory.svg";
+import productsSticker from "../../assets/stickers/Products.svg";
 import addSticker from "../../assets/stickers/addNew.svg";
 import wishlistSticker from "../../assets/stickers/wishlist.svg";
 import aboutSticker from "../../assets/stickers/about.svg";
@@ -8,61 +8,62 @@ import "./Home.scss";
 export default function Home() {
 	return (
 		<main className="homepage">
-			<div className="homepage__sticker-container homepage__sticker-container--one">
+			<div className="homepage__slogan">
+				<h1 className="homepage__slogan-title">creating time</h1>
+				<span className="homepage__slogan-title--accent">for fun</span>
+			</div>
+
+			<div className="homepage__sticker-container">
 				<Link to="products" className="homepage__link">
-					<div className="homepage__sticker-wrapper homepage__sticker-wrapper--inventory">
+					<div className="homepage__sticker-wrapper homepage__sticker-wrapper--products">
 						<img
-							src={inventorySticker}
-							alt="A decoration sticker"
-							className="homepage__sticker homepage__sticker--inventory"
+							src={productsSticker}
+							aria-hidden="true"
+							alt=""
+							className="homepage__sticker homepage__sticker--products"
 						/>
-						<span className="homepage__sticker-text homepage__sticker-text--inventory">
-							inventory
-						</span>
+						<h2 className="homepage__sticker-text homepage__sticker-text--products">
+							products
+						</h2>
+					</div>
+				</Link>
+				<Link to="products/add" className="homepage__link">
+					<div className="homepage__sticker-wrapper homepage__sticker-wrapper--add">
+						<img
+							src={addSticker}
+							aria-hidden="true"
+							alt=""
+							className="homepage__sticker homepage__sticker--add"
+						/>
+						<h2 className="homepage__sticker-text homepage__sticker-text--add">
+							add new
+						</h2>
 					</div>
 				</Link>
 				<Link to="wishlist" className="homepage__link">
 					<div className="homepage__sticker-wrapper homepage__sticker-wrapper--wishlist">
 						<img
 							src={wishlistSticker}
-							alt="A decoration sticker"
+							aria-hidden="true"
+							alt=""
 							className="homepage__sticker homepage__sticker--wishlist"
 						/>
-						<span className="homepage__sticker-text homepage__sticker-text--wishlist">
+						<h2 className="homepage__sticker-text homepage__sticker-text--wishlist">
 							wishlist
-						</span>
+						</h2>
 					</div>
 				</Link>
-			</div>
-			<div className="homepage__slogan">
-				<h1 className="homepage__slogan-title">creating time</h1>
-				<span className="homepage__slogan-title--accent">for fun</span>
-			</div>
-
-			<div className="homepage__sticker-container homepage__sticker-container--two">
-				<Link to="products/add" className="homepage__link">
-					<div className="homepage__sticker-wrapper homepage__sticker-wrapper--add">
-						<img
-							src={addSticker}
-							alt="A decoration sticker"
-							className="homepage__sticker homepage__sticker--add"
-						/>
-						<span className="homepage__sticker-text homepage__sticker-text--add">
-							add new
-						</span>
-					</div>
-				</Link>
-
 				<Link to="about" className="homepage__link">
 					<div className="homepage__sticker-wrapper homepage__sticker-wrapper--about">
 						<img
 							src={aboutSticker}
-							alt="A decoration sticker"
+							aria-hidden="true"
+							alt=""
 							className="homepage__sticker homepage__sticker--about"
 						/>
-						<span className="homepage__sticker-text homepage__sticker-text--about">
+						<h2 className="homepage__sticker-text homepage__sticker-text--about">
 							about
-						</span>
+						</h2>
 					</div>
 				</Link>
 			</div>
