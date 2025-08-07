44392eb1ee0e7429754522a3db1df99547d8bb93
Ife Onuorah <ifeonuorah@gmail.com>
2025-08-06 23:40:27 -0300

    Make main navigation header fixed positioned and adjust pages to accommodate for it

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -20,25 +20,27 @@ export default function Filter({
 	}
 	return (
 		<div className="filter">
-			<FilterItems
-				name="category"
-				content={categories}
-				selected={categoryFilter}
-				setSelected={setCategoryFilter}
-			/>
-			<FilterItems
-				name="status"
-				content={status}
-				selected={statusFilter}
-				setSelected={setStatusFilter}
-			/>
+			<div className="filter-wrapper">
+				<FilterItems
+					name="category"
+					content={categories}
+					selected={categoryFilter}
+					setSelected={setCategoryFilter}
+				/>
+				<FilterItems
+					name="status"
+					content={status}
+					selected={statusFilter}
+					setSelected={setStatusFilter}
+				/>
 
-			{statusFilter.length + categoryFilter.length > 0 && (
-				<button className="filter__reset" onClick={handleOnClick}>
-					{" "}
-					x Reset filters
-				</button>
-			)}
+				{statusFilter.length + categoryFilter.length > 0 && (
+					<button className="filter__reset" onClick={handleOnClick}>
+						{" "}
+						x Reset filters
+					</button>
+				)}
+			</div>
 		</div>
 	);
 }
